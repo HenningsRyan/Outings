179ac83907a71a4ed8c3e00fde4e711cffe72b91
Ryan Hennings <r.hennings11@gmail.com>
2017-10-11 14:26:05 -0700

    intial MapKit demo/PoC

--- Outings/ViewController.swift
+++ Outings/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // mapView.mapType = MKMapType.satellite
+        
+        let location = CLLocationCoordinate2DMake(37.336742774, -121.88154805451632)
+        let latitudeMeterSpan: CLLocationDegrees = 1000
+        let longitudeMeteresSpan: CLLocationDegrees = 1000
+        let mkCoordinate = MKCoordinateRegionMakeWithDistance(location, latitudeMeterSpan, longitudeMeteresSpan )
+        mapView.setRegion(mkCoordinate, animated: true)
+        
+        let sjsuPin = SJSUAnnotaion(title: "SJSU", subtitle: "Engineering Department", coordinate: location)
+        mapView.addAnnotation(sjsuPin)
+    }
+    
+    private struct Constants {
+        static let locationSJSU = CLLocationCoordinate2DMake(37.33689631985921, 121.88158728182316)
+        static let latitudinalMeters: CLLocationDegrees = 2000.0
+        static let longitudinalMeters: CLLocationDegrees = 2000.0
+    }
+    
 }
 
