24f0cd60e4fff0a0e02f5eb26988ccdfc5b402cb
Ryan Hennings <r.hennings11@gmail.com>
2017-10-25 13:04:18 -0700

    inital storyboard layout

---
+++ Outings/LoginVC.swift
@@ -0,0 +1,9 @@
+//
+//  LoginVC.swift
+//  Outings
+//
+//  Created by Ryan Hennings on 10/25/17.
+//  Copyright © 2017 OutingsCo. All rights reserved.
+//
+
+import Foundation
--- Outings/MapVC.swift
+++ Outings/MapVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
     
