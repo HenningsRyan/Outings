08f3198aec4f43e18142fdebdefe46288f1e37f2
rmit-s3603092-mitchell-sutton <s3603092@student.rmit.edu.au>
2017-11-12 15:26:24 -0800

    Further update to a non working program

--- Outings/AppDelegate.swift
+++ Outings/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import FBSDKCoreKit
+//import FBSDKCoreKit
 import Firebase
 
 @UIApplicationMain
@@ -15,11 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
-        FirebaseApp.configure()
-	FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+		FirebaseApp.configure()
+		//FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
 
 		return true
 	}
@@ -46,11 +45,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 	}
 	
-	func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+	/*func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
 		let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
 		
 		return handled
-	}
+	}*/
 
 
 }
